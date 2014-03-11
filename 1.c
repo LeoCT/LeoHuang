abfc3810a7ef4773e424236156d946f22f641ffa
LeoCT <Leo@cubietech.com>
2014-03-11 11:12:57 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,9 @@
-###%%( font-size: 150%; color: RED; )
-   Hello, world!
-   %%
+%%( font-size: 150%; color: RED; )
+Hello, world!
+%%
 
-##|| Heading 1 || Heading 2
-##| ''Gobble'' | Bar
-##| [Main]     | [SandBox]
+|| Heading 1 || Heading 2
+| ''Gobble'' | Bar
+| [Main]     | [SandBox]
+
+{{<nowiki>'''bold'''</nowiki>}}
